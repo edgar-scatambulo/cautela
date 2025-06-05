b744e854cdc490b4df46625f4b9ab354875658be
Edgar Scatambulo <edgar.scatambulo@gmail.com>
2025-06-05 12:36:06 +0000

    Remover o rótulo "Patrimônio:"

--- src/app/dashboard/policiais/page.tsx
+++ src/app/dashboard/policiais/page.tsx
@@ -273,7 +273,7 @@ export default function PoliciaisPage() {
                             <TableCell>
                                 <ul className="list-disc list-inside text-xs">
                                   {loan.equipment.map(eq => (
-                                    <li key={eq.id}>{eq.brand} (Patrimônio: {eq.serialNumber})</li>
+                                    <li key={eq.id}>{eq.brand} ({eq.serialNumber})</li>
                                   ))}
                                 </ul>
                             </TableCell>
